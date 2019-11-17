b269feb41269ef7f538c2ec37522c1a868907af9
Aqil <aqilabdaziz1123@gmail.com>
2019-11-17 21:14:08 +0800

    aqil

--- battle.pl
+++ battle.pl
@@ -1,2 +1,154 @@
+:- dynamic(digym/1).
+:- dynamic(udahheal/1).
+:- dynamic(maucapture/1).
+:- dynamic(udah_lari/1).
+:- dynamic(lagi_ketemu/1).
+:- dynamic(lagi_pick/1).
 :- dynamic(battle_status/1).
-battle_status(0).
+:- dynamic(finish_battle/1).
+
+:- include('player.pl').
+
+:- dynamic(current_tokemon1/4). 
+/*id tokemon kita, c_health, lvl*/
+:- dynamic(current_tokemon2/4). 
+/*id tokemon musuh, c_health, lvl*/
+
+digym(0).
+udahheal(0).
+finish_battle(0).
+
+fight :- lagi_ketemu(1), retract(lagi_ketemu(1)), assert(lagi_ketemu(0)),
+         retract(battle_status(0)), asserta(battle_status(1)),
+         write('Pilih tokemonmu dari tokemon yang tersedia!'),
+         !.
+
+fight :- 
+        lagi_ketemu(0),
+        write('Tidak ada Tokemon yang bisa dilawan!'),nl.
+
+          
+run :-  lagi_ketemu(0),
+        write('Tidak bisa lari jika pertarungannya tidak ada!'),nl,
+        write('Jangan mencoba lari dari kenyataan!'),nl,!.
+
+run :-  lagi_ketemu(1),
+        write('anda berlari seperti pengecut'), nl,
+        retract(lagi_ketemu(1)),asserta(lagi_ketemu(0)),
+        retract(current_tokemon2(A,B,C,D)), !.
+        
+
+
+tulis_battle :-  tokemon(A,_,_,_,_,C,X), current_tokemon1(X,D,_,_), tokemon(B,_,_,_,_,E,Y), current_tokemon2(Y,F,_,_), 
+                    write(A),nl, write('Health: '), write(D),nl, write('Type: '), write(C),nl,nl,
+                    write(B),nl, write('Health: '), write(F),nl, write('Type: '), write(E),nl,!.
+
+pick(X) :- battle_status(0), write('Tidak ada pertarungan saat ini'),nl,!.
+
+pick(X) :-  battle_status(1),
+            \+check_inv(X), write('Kamu tidak memiliki Tokemon itu!'),nl,
+            write('pilih Tokemon lain!'),nl,!.
+
+
+
+capture :- 
+            /* Kondisi prasyarat tak terpenuhi */
+            write('Anda mesti baru saja mengalahkan tokemon untuk capture!!'),nl,!.
+
+capture :- 
+            /* Kondisi prasyarat tak terpenuhi krn inven penuh*/
+            inventory_used(6),
+            write('Inventory penuh!! Drop salah satu tokemon pada inventory!'),nl,!.
+
+capture :- 
+            random(1,100,Y),
+            ((current_tokemon2(X,_,_), X>12) -> Y < 80 ; Y < 20),
+            write('Tokemon gagal di-capture'),nl,!.
+
+capture :-  
+            /* aksi di capture  */
+            !.
+
+capture :- 
+            write('Anda gagal men-capturenya!, better luck next time!!'), nl.
+
+see_result(X, Y) :-
+            /* melihat outcome dari battle */
+            X=\=0, Y=\=0, !.
+
+see_result(X, Y) :-
+            /* melihat outcome dari battle */
+            Y =:= 0, 
+            write('Selamat anda berhasil mengalahkan tokemon!'), nl,
+            write('Ingin mencoba untuk mencapture??'), nl,
+            retractall(finish_battle(0), finish_battle(1)),
+            !.
+
+see_result(X, Y) :-
+            /* melihat outcome dari battle */
+            X =:= 0, 
+            write('Tokemon anda mati!!'),
+            !.
+            
+calc_health :-  
+            /* menghitung health setelah suatu attack */
+            current_tokemon1(X1,E,Y1), current_tokemon2(X2,F,Y2),
+            retractall(current_tokemon1(_,_,_,_)),retractall(current_tokemon2(_,_,_,_)),
+            (E < 0 -> (assert(current_tokemon1(X1,0,Y1)), assert(current_tokemon1(X1,F,Y1))); 
+             (assert(current_tokemon1(X1,E,Y1)), assert(current_tokemon1(X1,F,Y1)))),
+            (F < 0 ->(assert(current_tokemon1(X1,E,Y1)), assert(current_tokemon1(X1,0,Y1)));
+            (assert(current_tokemon1(X1,E,Y1)), assert(current_tokemon1(X1,F,Y1)))),
+            tulis_battle,
+            see_result.
+/*currentTokemon(id,health,lvl,na)*/
+/*tokemon(nama,max health, nattack,sattack,namasatack,type,id)*/
+
+attack :-
+            battle_status(1),
+            current_tokemon1(A,B,C,D), current_tokemon2(E,F,G,H),
+            B2 is B - H, F2 is F-D, 
+            ((B2 >0, F2 > 0) ->
+            (retract(current_tokemon1(A,B,C,D)),retract(current_tokemon2(E,F,G,H)),
+            asserta(current_tokemon1(A,B2,C,D)),asserta(current_tokemon2(E,F2,G,H)),tulis_battle) ;
+            ((B2 > 0 , F2 =< 0) -> (write('YOU WONNNNNNN!!'),nl, retract(battle_status(1)), assert(battle_status(0)));(
+                    (B2 =< 0, F2 > 0) -> (write('u lose'),nl) ;write('go')
+            ) ))
+            ,!.
+
+specialattack :- 
+            battle_status(0),
+            write('Tidak sedang bertempur mas, mbak'),nl,!.
+
+specialattack :-
+            /* super effective condtion */
+            tokemon(Z,_,_,A,W,C,X), current_tokemon1(X,E,_), tokemon(_,_,B,_,_,D,Y), current_tokemon2(Y,F,_),
+            (C == "Grass" , D == "Water"; C == "Water" , D == "Fire"; C == "Fire" , D == "Grass"),
+            write(Z), write(" use "), write(W), write("!"),nl,
+            write("Wow! Super Effective."), nl,
+            dmg2 is A*15/10, E2 is E-B, F2 is F-dmg2,
+            retractall(current_tokemon1(_,_,_,_)),retractall(current_tokemon2(_,_,_,_)),
+            assert(current_tokemon1(X,E2,_)), assert(current_tokemon2(Y,F2,_)),
+            calc_health,
+            !.
+
+specialattack :- 
+            /* less effective condtion */
+            tokemon(Z,_,_,A,W,C,X), current_tokemon1(X,E,_), tokemon(_,_,B,_,_,D,Y), current_tokemon2(Y,F,_),
+            (C == "Grass" , D == "Water"; C == "Water" , D == "Fire"; C == "Fire" , D == "Grass"),
+            write(Z), write(" use "), write(W), write("!"),nl,
+            write("So sad! our attack isn't effective."), nl,
+            dmg2 is A*5/10, E2 is E-B, F2 is F-dmg2,
+            retractall(current_tokemon1(_,_,_,_)),retractall(current_tokemon2(_,_,_,_)),
+            assert(current_tokemon1(X,E2,_)), assert(current_tokemon2(Y,F2,_)),
+            calc_health,
+            !.
+
+specialattack :-
+            /* normal condtion */
+            tokemon(Z,_,_,A,W,C,X), current_tokemon1(X,E,_), tokemon(_,_,B,_,_,D,Y), current_tokemon2(Y,F,_),
+            write(Z), write(" use "), write(W), write("!"),nl,
+            E2 is E-B, F2 is F-A,
+            retractall(current_tokemon1(_,_,_,_)),retractall(current_tokemon2(_,_,_,_)),
+            assert(current_tokemon1(X,E2,_)), assert(current_tokemon2(Y,F2,_)),
+            calc_health,
+            !.
--- player.pl
+++ player.pl
@@ -1,61 +1,56 @@
 :- dynamic(player_status/1).
 :- dynamic(player_position/2).
-% :- dynamic(player_inv1/3).
-% :- dynamic(player_inv2/3).
-% :- dynamic(player_inv3/3).
-% :- dynamic(player_inv4/3).
-% :- dynamic(player_inv5/3).
-% :- dynamic(player_inv6/3).
 :- dynamic(inventory/7).                /* inventory(nama,max health, nattack,sattack,namasatack,type,id) */
 :- dynamic(maxInventory/1).             /* maxInventory(Maks) */
+:- dynamic(gameMain/1).
 
 :- include('tokemon.pl').
 :- include('utility.pl').
 
+
+check_inv(X):-
+	inventory(X,_,_,_,_,_,_).
+
+
+printInventory :-
+	inventory(A,B,C,D,E,F,G),
+	write(A),nl,
+	write(B),nl,
+	write(C),nl,
+	write(D),nl,
+	write(E),nl,
+	write(F),nl,
+	write(G),nl,!.
+
 init_player :-
 	asserta(gameMain(1)),
-	% lebarPeta(L),
-	% tinggiPeta(T),
-	% random(1,L,X),
-	% random(1,T,Y),
-	% asserta(player(X,Y)),
-	% asserta(healthpoint(100)),
-	% asserta(armor(0)),
-	% asserta(senjata(sniper_rifle,40,3)),
-	asserta(maxInventory(6)),
-	generateTokemon
-    % 
-    .
-
-
-cekPanjangInv(Panjang) :-
-	findall(B,inventory(B,_,_,_,_,_,_),ListBanyak),
-	length(ListBanyak,Panjang).
-
-addToInventory(_) :-
-	cekPanjangInv(Panjang),
-	maxInventory(Maks),
-    (Panjang+1) > Maks,
-    write("Inventory full!"),
-    nl,
-    !,
-    fail.
+	asserta(maxInventory(1)),
+	asserta(lagi_ketemu(0)),
+	asserta(battle_status(0)),
+	write('generating tokemon'),nl,
+	generateTokemon,!.
 
 addToInventory(Tokemon) :-
-    /*Inventory muat*/
+	maxInventory(X),
+	X2 is X+1,
+	retract(maxInventory(X)),
+	asserta(maxInventory(X2)),
     tokemon(Tokemon,H,N,S,NS,T,I),  
-	asserta(inventory(Tokemon,H,N,S,NS,T,I)),!.
+	assertz(inventory(Tokemon,H,N,S,NS,T,I)),!.
 
 
 delFromInventory(Tokemon) :-
 	\+inventory(Tokemon,_,_,_,_,_,_),!,fail.
+
+
 delFromInventory(Tokemon) :-
 	inventory(Tokemon,_,_,_,_,_,_),
 	retract(inventory(Tokemon,_,_,_,_,_,_)),
 	!.
 
-generateTokemon:-
+generateTokemon :-
 	random(1,12,X),
 	tokemon(Tokemon,_,_,_,_,_,X),
+	write('adding to inventory'),nl,
 	addToInventory(Tokemon),
 	!.
